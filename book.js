e9690913f579a11db7ad656f0381b575a512a9cd
azeem <ch.azim91@gmail.com>
2019-04-23 20:34:44 +0500

    stub

---
+++ book.js
@@ -0,0 +1,24 @@
+document.getElementById("stream").addEventListener("click", function(){
+    var element = document.getElementById("stream_meaning");
+    element.classList.toggle("mystyle");
+});
+
+document.getElementById("taxi").addEventListener("click", function(){
+    var element = document.getElementById("taxi_meaning");
+    element.classList.toggle("mystyle");
+});
+
+document.getElementById("spend").addEventListener("click", function(){
+    var element = document.getElementById("spend_meaning");
+    element.classList.toggle("mystyle");
+});
+
+document.getElementById("refer").addEventListener("click", function(){
+    var element = document.getElementById("refer_meaning");
+    element.classList.toggle("mystyle");
+});
+
+document.getElementById("offspring").addEventListener("click", function(){
+    var element = document.getElementById("offspring_meaning");
+    element.classList.toggle("mystyle");
+});
